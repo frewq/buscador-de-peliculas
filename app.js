f82f314f33f6dd78885f6f25bac4f1faababc624
frewq <fabi85_592@msn.com>
2019-03-31 07:42:43 -0300

    agregado: Flex, Lightbox

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+// TODO
+// Loguin (BBDD)
+// Puntuacion de peliculas (BBDD)
+// Descripciones y reseñas
+// Lightbox
+// REGEX para obtener imagenes de distinto tamaño
+
 const express = require('express');
 const app = express();
 const index = require('./routes/index')
@@ -15,7 +22,8 @@ app.use('/peliculas', (req, res) => {
   //consulta a la api OMDB 2
   let imagenURL = [];
   let titles = [];
-  let years = []
+  let years = [];
+  let altaCalidad = [];
 
   //api + encodeURIComponent + regexp + promesas + parsin
   axios.get(`http://www.omdbapi.com/?s=${encodeURIComponent(req.query.buscar).replace(/%20/g, "+")}&apikey=c61550c1`)
@@ -24,7 +32,8 @@ app.use('/peliculas', (req, res) => {
     arrayDatos = respuesta.data[Object.keys(respuesta.data)[0]];
     for (let i = 0; i < arrayDatos.length; i++) {
       // console.log('titulo: ', arrayDatos[i].Title);
-      // console.log('poster: ',npm  arrayDatos[i].Poster);
+      // console.log('poster: ', arrayDatos[i].Poster);
+      altaCalidad.push(String(arrayDatos[i].Poster).replace(/300.jpg$/i, "1200.jpg"))
       titles.push(arrayDatos[i].Title);
       imagenURL.push(arrayDatos[i].Poster);
       years.push(arrayDatos[i].Year);
@@ -36,7 +45,7 @@ app.use('/peliculas', (req, res) => {
 
   })
   .then( respo => {
-    res.render( 'peliculas', {busqueda: imagenURL, title: titles, year: years})
+    res.render( 'peliculas', {busqueda: imagenURL, title: titles, year: years, calidad: altaCalidad})
   })
 });
 
@@ -46,3 +55,10 @@ app.use('/', index);
 app.listen(port, function () {
   console.log('Ejemplo de app escuchando el puerto 3001.');
 });
+
+
+// let a = "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg";
+
+// a = a.replace(/300.jpg$/i, "2500.jpg");
+
+// console.log(a)
